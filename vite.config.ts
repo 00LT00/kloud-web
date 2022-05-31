c6913bc2871f0597c560942a0573611fd687de30
00LT00 <lightning@zerokirin.online>
2022-05-31 22:47:49 +0800

    mix output

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `kloud.js`,
+        chunkFileNames: `kloud.chunk.js`,
+        assetFileNames: 'kloud.[ext]',
+      },
+    },
+  },
   plugins: [react(), WindiCSS()],
 });
+
